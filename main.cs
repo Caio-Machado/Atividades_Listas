cc1b5c25c017adae667353612f0b40c0f56c1fec
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2020-10-20 22:57:47 +0000

    Final?

--- main.cs
+++ main.cs
@@ -15,7 +15,13 @@ class MainClass {
       }
     }
 
-    for (int y=0; y < Listas.Veiculos.Count; y++) 
+    Console.WriteLine("O Carro mais econômico é o {0}, com {1} km/l", carroMaisEco, contadorEco);
+
+    for (int y=0; y < Listas.Veiculos.Count; y++) {
+      Console.WriteLine("O Carro {0}, tem um consumo de {1}.", Listas.Veiculos[y], Listas.Consumos[y]);
+      Console.WriteLine("Seu carro consome {0} Litros de gasolina, para percorrer 1000 km.", (1000/Listas.Consumos[y]));
+      Console.WriteLine("O custo destes 1000 km é {0}.", (1000/Listas.Consumos[y])*4.89);
+    }
 
   }
 }
